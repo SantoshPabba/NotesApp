3ea2651b1287ad79bc81fb68271158dde6b20a5b
Santosh Pabba <Santosh.Pabba@gds.ey.com>
2023-03-14 00:51:11 +0530

    Removed Unused files

--- NotesWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NotesWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
